c66c5e98f0790317313ca81c3715601a77c866bb
Alex Sodemann <asodemann18@gmail.com>
2020-09-12 14:15:17 -0600

    Edit ProfileForm.scss file to actually us scss

--- src/ProfileForm/ProfileForm.tsx
+++ src/ProfileForm/ProfileForm.tsx
@@ -24,30 +24,27 @@ const ProfileForm: React.FC = () => {
 
   return (
 			<form className='profile-form'>
-				<label className='profile-label'>First Day of Your Last Period:</label>
+				<label>First Day of Your Last Period:</label>
 					<input
           name='lastOvulation'
-          className='profile-input'
 					type='date'
 					placeholder='mm/dd/yyyy'
 					value={lastOvulation}
 					aria-label='last-ovulation-input'
 					onChange={e => setLastOvulation(e.target.value)} 
 				  />
-        <label className='profile-label'>Average Cycle Length:</label>
+        <label>Average Cycle Length:</label>
 					<input
           name='avgCycleLength'
-          className='profile-input'
 					type='number'
 					placeholder='28 Days'
 					value={avgCycleLength}
 					aria-label='average-cycle-length-label'
 					onChange={e => setAvgCycleLength(e.target.value)} 
 					/>
-        <label className='profile-label'>Average Period Length:</label>
+        <label>Average Period Length:</label>
 					<input
           name='avgPeriodLength'
-          className='profile-input'
 					type='number'
 					placeholder='7 Days'
 					value={avgPeriodLength}
@@ -57,7 +54,6 @@ const ProfileForm: React.FC = () => {
 				<button 
           onClick={handleSubmit}
           type='button'
-          className='profile-button'
         >
           Submit
         </button>
