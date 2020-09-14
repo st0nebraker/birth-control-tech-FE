bed987dcdd5d13d73b8eed50c53c95f3aae061b0
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-09-14 13:07:01 -0600

    clicking calendar days find the days tracked info

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -9,22 +9,31 @@ import Login from '../Login/Login'
 import Profile from '../Profile/Profile';
 import Form from '../Form/Form';
 import Calendar from '../Calendar/Calendar';
-import { getUserData, submitUserData } from "../apiCalls";
+import { getUserData, submitUserData, getDays } from "../apiCalls";
 import './App.css';
+
+export interface Days {
+	Temperature: number,
+	Date: string,
+	highRisk: boolean
+}
 
 const App = () => {
 	const [ username, setUsername ] = useState('');
 	const [ loggedIn, setLoggedIn ] = useState(false);
-	const [lastOvulation, setLastOvulation] = useState('08-01-2020');
-  const [duration, setDuration] = useState(7);
-	const [averageCycle, setAverageCycle] = useState(28);
-	
+	const [ lastOvulation, setLastOvulation ] = useState('08-01-2020');
+  const [ duration, setDuration ] = useState(7);
+	const [ averageCycle, setAverageCycle ] = useState(28);
+	const [ days, setDays ] = useState<Days[]>([]);
+	const [ error, setError ] = useState("");
+
 	const logoutUser= () => {
 		setUsername('')
 		setLoggedIn(false)
 	}
 
 	useEffect(() => {getUserData()}, []);
+	useEffect(() => {getUserDays()}, []);
 
 	const postUserData = async (startDate: string, avgLength: number, avgCycle: number): Promise<any> => {
 		try {
@@ -35,14 +44,178 @@ const App = () => {
 		}
 	}
 
+	const getUserDays = async () => {
+		setDays([
+			{
+				Temperature: 97.3921407225235,
+				Date: '09/01/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.45472336125859,
+				Date: '09/02/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.37539305082102,
+				Date: '09/03/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.39594321187163,
+				Date: '09/04/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.22967507338001,
+				Date: '09/05/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.15550167462081,
+				Date: '09/06/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.37489920782173,
+				Date: '09/07/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.32587582389289,
+				Date: '09/08/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.2532495702544,
+				Date: '09/09/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.47893633165044,
+				Date: '09/10/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.12109556511471,
+				Date: '09/11/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.39352202710452,
+				Date: '09/12/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.39706814872008,
+				Date: '09/13/2020',
+				highRisk: false
+			},
+			{ 
+				Temperature: 97.20555830876744,
+				Date: '09/14/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.49626383244673,
+				Date: '09/15/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.24120211190903,
+				Date: '09/16/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 98.34677713289109,
+				Date: '09/28/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.70349779060184,
+				Date: '09/29/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 98.5701435459597,
+				Date: '09/30/2020',
+				highRisk: false
+			},
+			{
+				Temperature: 97.32789902753188,
+				Date: '09/17/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.33750832723287,
+				Date: '09/18/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.49237121174019,
+				Date: '09/19/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.10673653294982,
+				Date: '09/20/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.45035457538701,
+				Date: '09/21/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.39618222527686,
+				Date: '09/22/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.18218490258948,
+				Date: '09/23/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 98.31225964314442,
+				Date: '09/24/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 97.70977277629115,
+				Date: '09/25/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 98.5559779071062,
+				Date: '09/26/2020',
+				highRisk: true
+			},
+			{
+				Temperature: 98.29823570276363,
+				Date: '09/27/2020',
+				highRisk: true
+			}
+		])
+		// try {
+		// 	const data = await getDays();
+		// 	return setDays(data);
+		// } catch (error) {
+		// 	setError(error.toString());
+		// }
+	}
+
   return (
     <main className="App">
 			{loggedIn && <InfoTag username={username}/>}
 			<Switch>
 				<Route path='/info' component={Info} />
-				<Route path='/calendar' component={Calendar} />
 				<Route path='/new-entry' component={Form} />
 				<Route path='/stats' component={Reports} />
+				<Route 
+					path='/calendar' 
+					render={() => <Calendar userDays={days}/>} 
+				/>
 				<Route 
 					path='/profile'
 					render={() => <Profile lastOvulation={lastOvulation} duration={duration} averageCycle={averageCycle} logoutUser={logoutUser} postUserData={postUserData}/>}
--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,19 +1,32 @@
 import React, {useState} from 'react';
 import Calendar from 'react-calendar';
+import { Days } from '../App/App';
 import './Calendar.scss';
 import 'react-calendar/dist/Calendar.css'
 
+export interface CalendarProps {
+	userDays: Days[];
+}
 
+const CalendarPage: React.SFC<CalendarProps> = ({ userDays }) => {
+	const [ givenDate, setDate ] = useState<Date>(new Date());
+	const [ dayForDetails, setDayForDetails ] = useState<Days>();
+	
+	const clickedDayInfo = (e: any) => {
+		const dd = String(e.getDate()).padStart(2, '0');
+		const mm = String(e.getMonth() + 1).padStart(2, '0');
+		const yyyy = e.getFullYear();
+		const chosenDay = `${mm}/${dd}/${yyyy}`;
+		setDayForDetails(userDays.find(day => day.Date === chosenDay.toString()));
+	}
 
-const CalendarPage = () => {
-  const [givenDate, setDate] = useState<Date>(new Date());
-  
+	//popup new component w/ that days fertility info
   return(
    <main className='calendar'>
      <h1 className='headings'>Calendar</h1>
      <section className='calendar-container'>
       <Calendar
-          onClickDay={setDate}
+          onClickDay={(e) => clickedDayInfo(e)}
           value={givenDate}
           className='react-calendar'
         />
