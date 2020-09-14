d33ed4b3766c0b489a2d1f39d516cf1ae2074e40
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-09-14 15:54:22 -0600

    past days in calendar highlight high & low risk days

--- src/Calendar/Calendar.tsx
+++ src/Calendar/Calendar.tsx
@@ -1,9 +1,11 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import DayInfo from './DayInfo'
 import Calendar from 'react-calendar';
 import { Days } from '../App/App';
 import './Calendar.scss';
 import 'react-calendar/dist/Calendar.css'
+import moment from 'moment';
+// import { markTimeline } from 'console';
 
 export interface CalendarProps {
 	userDays: Days[];
@@ -12,7 +14,17 @@ export interface CalendarProps {
 const CalendarPage: React.SFC<CalendarProps> = ({ userDays }) => {
 	const [ givenDate, setDate ] = useState<Date>(new Date());
 	const [ dayForDetails, setDayForDetails ] = useState<Days>();
-	
+	const [ highRisk, setHighRisk ] = useState<Days[]>();
+	const [ lowRisk, setLowRisk ] = useState<Days[]>();
+
+	useEffect(() => { parseRiskDays() }, [ userDays ])
+	// useEffect(() => { parseRiskDays() }, [])
+
+	const parseRiskDays = () => {
+		setHighRisk(userDays.filter(day => day.highRisk));
+		setLowRisk(userDays.filter(day => !day.highRisk));
+	}
+
 	const clickedDayInfo = (e: any) => {
 		const dd = String(e.getDate()).padStart(2, '0');
 		const mm = String(e.getMonth() + 1).padStart(2, '0');
@@ -28,7 +40,16 @@ const CalendarPage: React.SFC<CalendarProps> = ({ userDays }) => {
       <Calendar
           onClickDay={(e) => clickedDayInfo(e)}
           value={givenDate}
-          className='react-calendar'
+					className='react-calendeda798ar'
+					tileClassName={({ date, view }) => {
+						if (highRisk && highRisk.find(x => x.Date === moment(date).format("MM/DD/YYYY"))) {
+							return 'red-risk';	
+						} else if (lowRisk && lowRisk.find(x => x.Date === moment(date).format("MM/DD/YYYY"))) {
+							return 'green-risk';	
+						} else {
+							return 'react-calendar'
+						}
+					}}
         />
      </section>
 			<DayInfo 
