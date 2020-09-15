badc9b9906660d0451da5b6119c34e1a280d8970
Becca Steinbrecher <beccasteinbrecher@gmail.com>
2020-09-15 13:38:12 -0600

    lint warnings fixed

--- src/Reports/Reports.tsx
+++ src/Reports/Reports.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
 import { Carousel } from 'react-responsive-carousel';
 import { Days } from '../App/App';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
